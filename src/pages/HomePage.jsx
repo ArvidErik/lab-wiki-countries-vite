1fa3211c01dfb988254614e6fd4916b47e73d64a
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-25 14:35:22 +0200

    D.O.N.E

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,18 @@
-function HomePage() {}
+import {Link} from "react-router-dom"
+
+import "./HomePage.css"
+
+function HomePage(props) {
+
+    console.log(props.countries);
+
+    return (
+        <div className="container">
+            {props.countries.map((country)=>{
+                return <Link key={country.alpha2Code} className="list-group-item list-group-item-action" to={`/${country.alpha3Code}`}>{country.name.common} <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt="" /></Link>
+            })}
+        </div>
+    )
+}
 
 export default HomePage;
